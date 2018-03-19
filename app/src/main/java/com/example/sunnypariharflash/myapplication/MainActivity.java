b2fc730fc5a227898a65208f5d4c2fa803299cb6
sunnyp123 <sunnyparihar35@gmail.com>
2018-03-19 16:10:54 +0530

    add button

--- app/src/main/java/com/example/sunnypariharflash/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sunnypariharflash/myapplication/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.sunnypariharflash.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    btn = findViewById(R.id.btn);
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "Button Message.", Toast.LENGTH_SHORT).show();
+        }
+    });
     }
 }
